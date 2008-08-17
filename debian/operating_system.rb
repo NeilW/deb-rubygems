a9b8b654830d30f820b088fa417f009e36128947
Neil Wilson <neil@brightbox.co.uk>
2008-08-17 17:50:45 +0100

    Fix copyright file.

--- debian/operating_system.rb
+++ debian/operating_system.rb
@@ -3,7 +3,16 @@
 #    
 #    This file is part of Rubygems packaging
 #
-#    Licensed unded the GPL. See /usr/share/common-licenses/GPL
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details
+#    (/usr/share/common-licenses/GPL-2).
 #
 require "rubygems/config_file"
 
